8fd69979485b405b56b1b41310e212a0f92cc4aa
Timon Private <timon.emken@gmail.com>
2025-10-13 21:42:28 +0200

    The simple renderer no longer supports reflections

--- src/RendererSimple.cpp
+++ src/RendererSimple.cpp
@@ -5,15 +5,10 @@ namespace Raytracer {
 Color RendererSimple::TraceRay(Ray ray, const Scene& scene) {
     auto intersection = Intersect(ray, scene);
     if (intersection) {
-        if (intersection->object && intersection->object->IsReflective()) {
-            Vector3D reflectDir = ray.GetDirection() - 2 * ray.GetDirection().Dot(intersection->normal) * intersection->normal;
-            Ray reflectRay(intersection->point, reflectDir);
-            return TraceRay(reflectRay, scene);
-        } else {
-            return intersection->object->GetColor(intersection->point);
-        }
+        return intersection->material->GetColor();
+    } else {
+        return scene.GetBackgroundColor();
     }
-    return scene.GetBackgroundColor();
 }
 
 }  // namespace Raytracer
