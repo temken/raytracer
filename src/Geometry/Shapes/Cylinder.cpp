1e4286d49a856cd14f147094daf52aafd4185b09
Timon Private <timon.emken@gmail.com>
2025-11-02 01:22:44 +0100

    Add cylinder implemention

--- src/Geometry/Shapes/Cylinder.cpp
+++ src/Geometry/Shapes/Cylinder.cpp
@@ -1,5 +1,35 @@
 #include "Geometry/Shapes/Cylinder.hpp"
 
+#include "Geometry/Shapes/Disk.hpp"
+#include "Geometry/Shapes/Tube.hpp"
+
+#include <iostream>
+#include <memory>
+
 namespace Raytracer::Geometry {
+
+Cylinder::Cylinder(const Vector3D& position, const Vector3D& orientation, double radius, double height) :
+    CompositeShape(Shape::Type::CYLINDER, position, orientation),
+    mRadius(radius),
+    mHeight(height) {
+    // Create and add top disk
+    Vector3D topCenter = mPosition + (mHeight / 2.0) * orientation;
+    auto topDisk = std::make_shared<Disk>(topCenter, orientation, mRadius);
+    AddComponent(topDisk);
+    // Create and add bottom disk
+    Vector3D bottomCenter = mPosition - (mHeight / 2.0) * orientation;
+    auto bottomDisk = std::make_shared<Disk>(bottomCenter, -1.0 * orientation, mRadius);
+    AddComponent(bottomDisk);
+    // Create and add mantle (open cylinder)
+    auto mantle = std::make_shared<Tube>(position, orientation, radius, height);
+    AddComponent(mantle);
+}
+
+void Cylinder::PrintInfo() const {
+    PrintInfoCompositeBase();
+    std::cout << "\tRadius: " << mRadius << std::endl
+              << "\tHeight: " << mHeight << std::endl
+              << std::endl;
+}
 
 }  // namespace Raytracer::Geometry
--- src/Geometry/Shapes/Cylinder.hpp
+++ src/Geometry/Shapes/Cylinder.hpp
@@ -1,10 +1,18 @@
 #pragma once
 
+#include "Geometry/CompositeShape.hpp"
+
 namespace Raytracer::Geometry {
 
-class Cylinder {
+class Cylinder : public CompositeShape {
 public:
+    Cylinder(const Vector3D& position, const Vector3D& orientation, double radius, double height);
+
+    virtual void PrintInfo() const override;
+
 private:
+    double mRadius;
+    double mHeight;
 };
 
 }  // namespace Raytracer::Geometry
