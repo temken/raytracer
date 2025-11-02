2f06d4162c5efa62d3471fd9c85bd6c6c4df5eb7
Timon Private <timon.emken@gmail.com>
2025-11-02 01:23:05 +0100

    Add axis aligned box

--- src/Geometry/Shapes/BoxAxisAligned.hpp
+++ src/Geometry/Shapes/BoxAxisAligned.hpp
@@ -1,10 +1,21 @@
 #pragma once
 
+#include "Geometry/CompositeShape.hpp"
+#include "Geometry/Vector.hpp"
+
 namespace Raytracer::Geometry {
 
-class BoxAxisAligned {
+class BoxAxisAligned : public CompositeShape {
 public:
+    BoxAxisAligned(const Vector3D& center, double length, double width, double height);
+
+    virtual void PrintInfo() const override;
+
 private:
+    double mLength, mWidth, mHeight;
+
+    // Rotation method deleted to enforce axis-alignment
+    void Spin(double angle, OrthonormalBasis::BasisVector axis) = delete;
 };
 
 }  // namespace Raytracer::Geometry
